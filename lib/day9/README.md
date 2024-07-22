c238d5d7d3159d9edee62e3d2d3e017c58ead3c8
Dubem <dubembc001@gmail.com>
2024-07-22 07:39:11 +0100

    Add Day 9 content : Switch Statements

---
+++ lib/day9/README.md
@@ -0,0 +1,9 @@
+# dart_100_days_code_challenge
+
+Today, I learned the DART Control flow, specifically focusing on `switch`statement.
+
+- `switch` evaluates an expression and matches it to one of the specified `case` values.
+
+- `break` is necessary to exit the switch statement after executing the corresponding code block.
+
+- `default` is optional and runs if no cases match the expression.
---
+++ lib/day9/switchStatement.dart
@@ -0,0 +1,31 @@
+void main() {
+  int day = 5;
+
+  switch (day){
+    case 1:
+    print('monday');
+    break;
+    case 2:
+    print('tuesday');
+    break;
+    case 3:
+    print('wednesday');
+    break;
+    case 4:
+    print('thursday');
+    break;
+    case 5:
+    print('friday');
+    break;
+    case 6:
+    print('saturday');
+    break;
+    case 7:
+    print('sunday');
+    break;
+    default:
+    print('Invalid day.');
+  }
+}
+
+//Output = "Friday"
